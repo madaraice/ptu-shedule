a66964c56750b1a670de899c358bf883282c216e
Vladislav Grebenyuk <vladislav.grebenyuk.00@mail.ru>
2020-01-12 04:01:38 +0300

    [master] [add] api

---
+++ PkghShedule/Api/Controllers/SheduleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SheduleParser;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SheduleController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var shedule = await Parser.Parse();
+            return shedule;
+        }
+
+        [HttpGet("{groupName}")]
+        public async Task<string> Get(string groupName)
+        {
+            var shedule = await Parser.Parse(groupName);
+            return shedule;
+        }
+    }
+}
